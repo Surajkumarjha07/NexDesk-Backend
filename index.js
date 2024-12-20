e840bb0c0ead569ffd7945092b09c947ae56d725
Suraj kumar jha <surajkumarjha771@gmail.com>
2024-12-20 20:22:08 +0530

    Realtime features are in progress

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ io.on('connection', (socket) => {
         }
         EmailToRoom.set(email, meetingCode);
         if (!RoomToEmail.has(meetingCode)) {
-            RoomToEmail.set(meetingCode,[]);
+            RoomToEmail.set(meetingCode, []);
         }
         RoomToEmail.get(meetingCode).push(email);
         socket.join(meetingCode);
@@ -76,7 +76,7 @@ io.on('connection', (socket) => {
         io.to(socket.id).emit("roomJoined", email, meetingCode);
         EmailToRoom.set(email, meetingCode);
         if (!RoomToEmail.has(meetingCode)) {
-            RoomToEmail.set(meetingCode,[]);
+            RoomToEmail.set(meetingCode, []);
         }
         RoomToEmail.get(meetingCode).push(email);
     })
@@ -90,8 +90,44 @@ io.on('connection', (socket) => {
         console.log("room: ", RoomToEmail.get(meetingCode));
     })
 
-    socket.on("itemDraw", (meetingCode,) => {
-        
+    socket.on("itemDraw", (data) => {
+        const { meetingCode, id, x, y, width, height, shapeColor, shapeType, patternType, borderType, opacity, resize } = data;
+        socket.broadcast.to(meetingCode).emit("itemDrawed", { id, x, y, width, height, shapeColor, shapeType, patternType, borderType, opacity, resize });
+    })
+
+    socket.on("itemSelect", (data) => {
+        const { meetingCode, id } = data;
+        socket.broadcast.to(meetingCode).emit("itemSelected", id);
+    })
+
+    socket.on("itemUnSelect", (meetingCode) => {
+        socket.broadcast.to(meetingCode).emit("itemUnSelected");
+    })
+
+    socket.on("itemErase", (data) => {
+        const { meetingCode, id } = data;
+        socket.broadcast.to(meetingCode).emit("itemErased", id);
+    })
+
+    socket.on("itemModify", (data) => {
+        const { meetingCode, id, shapeColor, patternType, borderType, opacity } = data;
+        socket.broadcast.to(meetingCode).emit("itemModified", { id, shapeColor, patternType, borderType, opacity })
+    })
+
+    socket.on("itemMoving", (data) => {
+        const { meetingCode, id, x, y } = data;
+        console.log(data);
+        socket.broadcast.to(meetingCode).emit("itemMoved", { id, x, y })
+    })
+
+    socket.on("itemHeightResize", (data) => {
+        const { meetingCode, id, newHeight } = data;
+        socket.broadcast.to(meetingCode).emit("itemHeightResized", {id, newHeight});
+    })
+
+    socket.on("itemWidthResize", (data) => {
+        const { meetingCode, id, newWidth } = data;
+        socket.broadcast.to(meetingCode).emit("itemWidthResized", {id, newWidth});
     })
 
     socket.on('disconnect', () => {
