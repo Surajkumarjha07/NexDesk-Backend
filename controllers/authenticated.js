dd133c16c6c3a945279a905562378e7d2fd1797f
Suraj kumar jha <surajkumarjha771@gmail.com>
2025-01-14 20:18:19 +0530

    Fixed Login router

--- controllers/authenticated.js
+++ controllers/authenticated.js
@@ -3,26 +3,27 @@ require("dotenv").config();
 
 async function userAuthenticated(req, res) {
     try {
-        const token = req.cookies.authtoken || req.headers["authorization"]?.split(" ")[1];
+        const token = req.cookies.authtoken || req.headers["authorization"].split(" ")[1];
         if (!token) {
             return res.status(401).json({
-                message: "Token expired or missing"
-            });
+                message: "token expired"
+            })
         }
 
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         if (verified) {
             return res.status(200).json({
-                message: "Authorized: Token valid",
-            });
-        } else {
-            console.log("User unauthorized - Invalid token");
+                message: "authorized: Token valid",
+            })
+        }
+        else {
+            console.log("user unauthorized");
             return res.status(403).json({
                 message: "Unauthorized: Token invalid",
             });
         }
     } catch (error) {
-        console.error("Error during token verification:", error);
+        console.log("error: ", error.message);
         return res.status(500).json({
             message: "Internal server error",
         });
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "https://nexdesk.onrender.com",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "PATCH", "OPTIONS"],
     credentials: true,
 };
