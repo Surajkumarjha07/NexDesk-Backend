a2a936f2856e00a3ec5465e0cb9f76c46479c203
Suraj kumar jha <surajkumarjha771@gmail.com>
2025-01-15 22:32:01 +0530

    App is complete and may be upgraded in future

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.options("*", cors(corsOptions));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Origin", "https://your-frontend-domain.com"); // Replace with your frontend domain
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Origin", "https://nexdesk.onrender.com"); 
+    res.header("Access-Control-Allow-Methods", "GET", "POST", "PUT", "DELETE", "HEAD", "PATCH", "OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 });
