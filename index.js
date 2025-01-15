46fa831a6afa3cb87981e24493bbe5fb0aa1b058
Suraj kumar jha <surajkumarjha771@gmail.com>
2025-01-15 22:27:21 +0530

    App is complete and may be upgraded in future

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Origin", "https://your-frontend-domain.com"); // Replace with your frontend domain
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 ConnectDatabase()
 
 app.get("/", (req, res) => {
